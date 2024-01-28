b2fd0a22febae041bd22db466db5184d3d53bc8d
PhuocPhann162 <21520405@gm.uit.edu.vn>
2024-01-29 00:02:13 +0700

    styling menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
     extend: {},
   },
   plugins: [],
